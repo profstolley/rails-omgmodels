24a41aba18a0bc7d4a3d794cb7116e6bd0f5cf95
Karl Stolley <karl.stolley@gmail.com>
2016-09-19 19:00:10 -0500

    Added validation on :title for Note
    Also added test to ensure the validation rule works

--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "notes must have a title" do
+    note = Note.new(body: "I have no title")
+    assert note.invalid?, "note should not be valid without a title"
+  end
 end
