67db1ae10d2773655357ad5eb7c145636f6d704f
Karl Stolley <karl.stolley@gmail.com>
2016-09-26 22:44:31 -0500

    Added private set_notebook method to run before index, new, and create

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,6 @@
 class NotesController < ApplicationController
   before_action :set_note, only: [:show, :edit, :update, :destroy]
+  before_action :set_notebook, only: [:index, :new, :create]
 
   # GET /notebooks/:id/notes
   # GET /notebooks/:id/notes.json
@@ -65,6 +66,9 @@ class NotesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_note
       @note = Note.find(params[:id])
+    end
+    def set_notebook
+      @notebook = Notebook.find(params[:notebook_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
