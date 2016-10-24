1728827dbd919fd4d38df94e3683fd049a248d87
Karl Stolley <karl.stolley@gmail.com>
2016-10-24 18:07:17 -0500

    Modified migration to require and index :uid and :provider

--- db/migrate/20161024230019_create_users.rb
+++ db/migrate/20161024230019_create_users.rb
@@ -4,10 +4,15 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.string :nickname
       t.string :name
       t.string :location
-      t.string :uid
-      t.string :provider
+      t.string :uid, null: false
+      t.string :provider, null: false
 
       t.timestamps
     end
+
+    add_index :users, :provider
+    add_index :users, :uid
+    add_index :users, [:provider, :uid], unique: true
+
   end
 end
