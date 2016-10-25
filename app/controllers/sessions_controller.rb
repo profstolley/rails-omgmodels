fe2a26ce51c49c69d9f2929e4eeda513f5bccb06
Karl Stolley <karl.stolley@gmail.com>
2016-10-24 20:22:21 -0500

    Added controller and model code to build User record from auth hash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 class SessionsController < ApplicationController
   def create
-    render text: request.env['omniauth.auth'].to_yaml
+    # render text: request.env['omniauth.auth'].to_yaml
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    session[:id] = @user.id
+    redirect_to notebooks_path
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+
+  def self.from_omniauth(hash)
+    user = User.find_or_create_by(uid: hash['uid'], provider: hash['provider'])
+    user.name = hash['info']['name']
+    user.nickname = hash['info']['nickname']
+    user.image = hash['info']['image']
+    user.location = hash['info']['location']
+    user.save!
+    user
+  end
+
 end
