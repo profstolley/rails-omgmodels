78e07417dc9c94304a7bd2d9c9b0b9c7568aa5ac
Karl Stolley <karl.stolley@gmail.com>
2016-10-24 20:07:35 -0500

    Enforce the presence of uid and provider, and create indices on them

--- db/migrate/20161025010206_create_users.rb
+++ db/migrate/20161025010206_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :uid
-      t.string :provider
+      t.string :uid, null: false
+      t.string :provider, null: false
       t.string :nickname
       t.string :name
       t.string :location
@@ -10,5 +10,10 @@ class CreateUsers < ActiveRecord::Migration[5.0]
 
       t.timestamps
     end
+    
+    add_index :users, :provider
+    add_index :users, :uid
+    add_index :users, [:provider, :uid], unique: true
+    
   end
 end
