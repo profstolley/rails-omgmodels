19daef7527a571e7fc5c2b9c1e092ed42bed1307
Karl Stolley <karl.stolley@gmail.com>
2016-11-07 17:39:32 -0600

    Modified Notes controller to permit :image params

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -80,6 +80,6 @@ class NotesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def note_params
-      params.require(:note).permit(:title, :body)
+      params.require(:note).permit(:title, :body, :image)
     end
 end
